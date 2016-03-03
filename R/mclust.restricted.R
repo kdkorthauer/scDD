ba25512df862bac6c28456ae854b090e747c5a38
kdkorthauer <kdkorthauer@gmail.com>
2016-03-03 09:50:44 -0500

    increase robustness of error checking in clustering fn

--- R/mclust.restricted.R
+++ R/mclust.restricted.R
@@ -17,6 +17,7 @@
 #' 
 #' @return List object with (1) vector of cluster membership, (2) cluster means, (3) cluster variances, (4) number of model parameters,
 #'  (5) sample size, (6) BIC of selected model, and (6) loglikelihood of selected model. 
+#'  @export
 
 
 
@@ -43,26 +44,40 @@ mclustRestricted <- function(y, restrict=TRUE){
       min.cl <- which.min(mc$parameters$variance$sigmasq)
       vardiff <- mc$parameters$variance$sigmasq[max.cl] / mc$parameters$variance$sigmasq[min.cl]
       nmin <- table(cl)[min.cl]
-      
+      mincat <- min(table(cl))
       tries <- 0	
       
       if (length(vardiff)==0){
         vardiff <- 1
+      }else if(is.na(vardiff)){
+        vardiff <- 1
       }
       if (length(nmin)==0){
+        nmin <- Inf
+      }else if(is.na(nmin)){
         nmin <- Inf
       }
       if (length(meandiff)==0){
         meandiff <- Inf
+      }else if(is.na(meandiff)){
+        meandiff <- Inf
+      }
+      if (length(mincat)==0){
+        mincat <- 1
+      }else if(is.na(mincat)){
+        mincat <- 1
       }
       
-      # enforce that clusters have to be separated by at least 2 sds 
-      while( (min(meandiff) < 2 | (vardiff > 3 & nmin < 0.10*length(y))) & min(table(cl))>2 & tries <=4 & sum(is.na(mc$class))==0){
+      err <- 0
+      # enforce that clusters have to be separated by at least 2 sds - if not, decrease possible components by 1 and refit
+      while( (min(meandiff) < 2 | (vardiff > 3 & nmin < 0.10*length(y))) & mincat>2 & tries <=4 & sum(is.na(mc$class))==0 & err==0){
+        err <- 0
         comps_old <- comps
         comps <- comps-1 
         mc <- Mclust(y, warn=FALSE, modelNames=c("V"), G=1:comps)
         cl <- mc$classification
         comps <- mc$G
+        mincat <- min(table(cl))
         
         tries <- tries + 1
         if (comps > 1 & comps_old-comps==1){
@@ -78,15 +93,45 @@ mclustRestricted <- function(y, restrict=TRUE){
           vardiff <- -Inf
           nmin <- 100
         }
+        
+        # check for errors in new fit
         if (length(vardiff)==0){
           vardiff <- 1
+          err <- 1
+        }else if(is.na(vardiff)){
+          vardiff <- 1
+          err <- 1
         }
         if (length(nmin)==0){
           nmin <- Inf
+          err <- 1
+        }else if(is.na(nmin)){
+          nmin <- Inf
+          err <- 1
         }
         if (length(meandiff)==0){
           meandiff <- Inf
+          err <- 1
+        }else if(is.na(mean.diff)){
+          meandiff <- Inf
+          err <- 1
         }
+        if (length(mincat)==0){
+          mincat <- 1
+          err <- 1
+        }else if(is.na(mincat)){
+          mincat <- 1
+          err <- 1
+        }
+        
+        # if fit resulted in any errors, revert to previous fit
+        if (err == 1){
+          comps <- comps+1 
+          mc <- Mclust(y, warn=FALSE, modelNames=c("V"), G=1:comps)
+          cl <- mc$classification
+          comps <- mc$G
+        }
+        
       }
     }
   }
